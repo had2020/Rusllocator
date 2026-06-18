80e2e739fd2ff2ba61338b0cea5a47b3731104bb
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2026-06-18 15:22:22 -0700

    new update with a better BitMapAllocator

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+#![no_std]
+
+#[repr(C)] 
+pub struct BitMapAllocator {
+    pub bitmap: usize,
+}
+
+impl BitMapAllocator {
+    pub fn alloc(&mut self, page_size: usize) -> *mut usize {
+        let mut addr: usize = self.bitmap >> self.bitmap.trailing_ones();
+
+        self.bitmap = self.bitmap ^ (1 << addr);
+
+        (addr * page_size) as *mut usize
+    }
+}
+
+/*
 /// bitmap where 0 = free and 1 = used.
 #[repr(C, align(64))]
 pub struct PageBitmap<const PAGE_BYTES: u32> {
@@ -50,3 +68,77 @@ macro_rules! new_pagebitmap {
         }
     };
 }
+*/
+
+/*
+use core::alloc::{self, GlobalAlloc, Layout};
+use core::sync::atomic::{AtomicUsize, Ordering};
+use core::ptr::null_mut;
+use core::usize;
+
+#[repr(align(64))]
+struct Allocator<const BYTES: usize> {
+    heap: [u8: BYTES],
+    next: AtomicUsize,
+}
+
+impl<const BYTES: usize> Allocator<BYTES> {
+    const fn new() -> Self {
+        Self {
+            heap: [const {0_u8}; BYTES],
+            next: AtomicUsize::new(0)
+        }
+    }
+}
+*/
+/* Not chache aligned
+use core::usize;
+
+#[repr(C, align(64))]
+pub struct BitMapAllocator<const PAGES: usize, const PAGE_SIZE_BTYES: usize> {
+    bitmap: [u8; PAGES],
+}
+
+impl<const PAGES: usize, const PAGE_SIZE_BTYES: usize> BitMapAllocator<PAGES, PAGE_SIZE_BTYES> {
+    pub const fn new() -> Self {
+        Self { bitmap: [0; PAGES] }
+    }
+
+    #[inline(always)]
+    pub fn alloc(&mut self) -> usize {
+        /*
+        let addr: usize;
+        let mut i: u8 = 1;
+        loop {
+            if (self.bitmap << i).trailing_zeros() < (self.bitmap << (i - 1)).trailing_zeros() {
+                addr = i as usize * PAGE_BYTES;
+                break;
+            }
+
+            i += 1;
+        }
+        addr
+        */
+
+        /*
+        loop {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            {
+                core::arch::asm!()
+            }
+
+            #[cfg(any(target_arch = "riscv64", target_arch = "riscv32"))]
+            {}
+
+            #[cfg(any(target_arch = "aarch64"))]
+            {}
+        }
+        */
+        let addr: usize = 0;
+        loop {
+            // TODO: use AND and XOR to make this branchless
+
+        }
+    }
+}
+*/
